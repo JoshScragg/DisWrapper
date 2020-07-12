48e281eb9ef8e21f50d24c4aef5648bc2c9a1d7f
Josh Scragg <josh@scragg.co.nz>
2020-07-12 17:18:29 +1200

    hopefully last pypi related commit

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from distutils.core import setup
 setup(
   name = 'DisWrapper',         # How you named your package folder (MyLib)
   packages = ['DisWrapper'],   # Chose the same as "name"
-  version = '0.1.6',      # Start with a small number and increase it with every change you make
+  version = '0.1.7',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Small python wrapper for discord',   # Give a short description about your library
   author = 'Josh Scragg',                   # Type in your name
   author_email = 'josh@scragg.co.nz',      # Type in your E-Mail
   url = 'https://github.com/JoshScragg/DisPy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/JoshScragg/DisWrapper/archive/0.1.5.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/JoshScragg/DisWrapper/archive/0.1.6.tar.gz',    # I explain this later on
   keywords = ['Discord', 'Wrapper', 'Bot', 'Selfbot', 'Dis', 'library'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'websocket',
           'requests',
+          'websocket-client',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
